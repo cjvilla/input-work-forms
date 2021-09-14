4d0d129ce88330497c3668f3bcaa7537d22b0fa0
cjvilla <CelineVillareal2020@u.northwestern.edu>
2021-09-14 16:51:39 -0500

    add excel file and dataframe

--- formBuilder.py
+++ formBuilder.py
@@ -1,13 +1,24 @@
 import PySimpleGUI as sg
+import pandas as pd
 
 sg.theme('DarkTeal9')
 
+EXCEL_FILE = 'Data_Entry.xlsx'
+df = pd.read_excel(EXCEL_FILE)
+
 layout = [
     [sg.Text('Please fill out the following fields:')],
-    [sg.Text('Name', size=(100, 1)), sg.InputText(key='Name')],
-    [sg.Submit(), sg.Exit()]
+    [sg.Text('Name', size=(15,1)), sg.InputText(key='Name')],
+    [sg.Text('City', size=(15,1)), sg.InputText(key='City')],
+    [sg.Text('Favorite Colour', size=(15,1)), sg.Combo(['Green', 'Blue', 'Red'], key='Favorite Colour')],
+    [sg.Text('I speak', size=(15,1)),
+                            sg.Checkbox('German', key='German'),
+                            sg.Checkbox('Spanish', key='Spanish'),
+                            sg.Checkbox('English', key='English')],
+    [sg.Text('No. of Children', size=(15,1)), sg.Spin([i for i in range(0,16)],
+                                                       initial_value=0, key='Children')],
+    [sg.Submit(), sg.Button('Clear'), sg.Exit()]
 ]
-
 window = sg.Window('Simple data entry form', layout)
 
 while True:
