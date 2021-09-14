d6e1e9cc0f94e67828f72925b23249f6ef56ebcb
cjvilla <CelineVillareal2020@u.northwestern.edu>
2021-09-14 16:46:03 -0500

    add simple entry form

---
+++ formBuilder.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+sg.theme('DarkTeal9')
+
+layout = [
+    [sg.Text('Please fill out the following fields:')],
+    [sg.Text('Name', size=(100, 1)), sg.InputText(key='Name')],
+    [sg.Submit(), sg.Exit()]
+]
+
+window = sg.Window('Simple data entry form', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Submit':
+        print(event, values)
+window.close()
